e813a0ea9e6e2735d80d7e634b4857f38a217c3f
Nehal Nevle <nehalsuperdude@gmail.com>
2021-04-29 23:59:14 +0530

    inverse kinematics

--- catkin_ws/src/arm_move/scripts/arm_move.py
+++ catkin_ws/src/arm_move/scripts/arm_move.py
@@ -1,19 +1,64 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float64
+import math
 
 def talker():
-    pub = rospy.Publisher('/arm/joint1_position_controller/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/arm/joint3_position_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/arm/joint1_position_controller/command', Float64, queue_size=10)
+    pub3 =  rospy.Publisher('/arm/joint2_position_controller/command', Float64, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(1) # 10hz
     msg = Float64()
-    msg.data= 2.0
-    while not rospy.is_shutdown():
-        rospy.loginfo(msg)
-        pub.publish(msg)
-        rate.sleep()
-        #msg.data+=0.1
-    #rospy.spin()
+    x= float(input("input x:"))
+    y= float(input("input y:"))
+    z= float(input("input z:"))
+    # Calculating theta1
+    if (x)!=0:
+        theta1=math.atan(y/x) + (math.pi/2)
+    else:
+        theta1=math.pi/2 + (math.pi/2)
+    msg.data= theta1
+    rospy.loginfo(msg)
+    pub1.publish(msg)
+    rate.sleep()
+    pub1.publish(msg)
+    rate.sleep()
+    pub1.publish(msg)
+    rate.sleep()
+    # Calculating theta2
+    
+    hypo1=math.sqrt((x**2)+(y**2)) 
+    left= z - 0.5
+    phi2=math.atan(left/hypo1)#(math.pi/2)
+    hypo2=math.sqrt((hypo1**2)+(left**2))
+    num1=(((2.5)**2)-(3**2)-(hypo2**2))
+    deno1=(-1*2*hypo2*3)
+    print(num1/deno1)
+    phi=math.acos(num1/deno1)
+    theta2=phi-phi2
+    msg.data= theta2 + (math.pi/2)
+    num2=(((hypo2)**2)-(3**2)-((2.5)**2))
+    deno2=(-1*2*2.5*3)
+    phi3=math.acos(num2/deno2)
+    theta3=(2*(math.pi))-phi3
+    msg.data= theta3 - (math.pi)
+    rospy.loginfo(msg)
+    pub3.publish(msg)
+    rate.sleep()
+    pub3.publish(msg)
+    rate.sleep()
+    pub3.publish(msg)
+    msg.data= theta2 + (math.pi/2)
+    rospy.loginfo(msg)
+    pub2.publish(msg)
+    rate.sleep()
+    pub2.publish(msg)
+    rate.sleep()
+    pub2.publish(msg)
+    rate.sleep()
+    
+    
 if __name__ == "__main__":
     
      try:
